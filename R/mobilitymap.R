6b28948e74775c1de02d88a9ad20b09a48e260bf
Pachka <46706800+Pachka@users.noreply.github.com>
2019-03-25 23:31:48 +0100

    Update mobilitymap.R

--- R/mobilitymap.R
+++ R/mobilitymap.R
@@ -2,15 +2,14 @@
 #'
 #' This function produces a map illustrating the mobility network.
 #'
-#' Cette fonction sert a produire une carte de mobilité.
+#' Cette fonction sert a produire une carte de mobilite.
 #'
-#' @import rworldmap
-#' @importFrom graphics points
+#' @import rworldmap graphics
 #' @export mobilitymap
 #' @param x the data.frame containing the degree (mobility parameter) and the GPS coordiantes (column names: LONGITUDE_X and LATITUDE_Y) \cr
-#' le data.frame contenant le degré (paramètre de mobilité) et les coodonnées GPS (nom des colonnes : LONGITUDE_X et LATITUDE_Y)
+#' le data.frame contenant le degre (parametre de mobilite) et les coodonnees GPS (nom des colonnes : LONGITUDE_X et LATITUDE_Y)
 #' @param y  (par defaut, 0.8) the size of the localities. It can be a parameters contained in x.
-#' @keywords plot ggplot sna réseau
+#' @keywords plot ggplot sna network
 #' @usage mobilitymap(x,y)
 #'
 mobilitymap <- function(x, y = 0.8){
