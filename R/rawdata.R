974e50e12f848235d700d24b0f9f9659853cf3bc
Pachka <46706800+Pachka@users.noreply.github.com>
2019-03-25 23:49:24 +0100

    Create rawdata.R

---
+++ R/rawdata.R
@@ -0,0 +1,15 @@
+#' Fake dataset for animal movements between Tunisia and other countries
+#'
+#' Random data generated as an example of inputs required for the analysis.
+#'
+#' @docType data
+#'
+#' @usage data(rawdata)
+#'
+#' @format An object of class dataframe.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' head(rawdata)
+"rawdata"
