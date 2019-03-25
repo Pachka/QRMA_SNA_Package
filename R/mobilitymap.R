8c554738786cb6aebcd7c3c9515d69386a0cdd33
Pachka <46706800+Pachka@users.noreply.github.com>
2019-03-26 00:01:12 +0100

    update mobilitymap

--- R/mobilitymap.R
+++ R/mobilitymap.R
@@ -4,14 +4,13 @@
 #'
 #' Cette fonction sert a produire une carte de mobilite.
 #'
-#' @import rworldmap graphics arulesViz
+#' @import rworldmap graphics
 #' @export mobilitymap
-#' @param x the data.frame containing the degree (mobility parameter) and the GPS coordiantes (column names: LONGITUDE_X and LATITUDE_Y) \cr
-#' le data.frame contenant le degre (parametre de mobilite) et les coodonnees GPS (nom des colonnes : LONGITUDE_X et LATITUDE_Y)
+#' @param x the data.frame containing the degree (mobility parameter) and the GPS coordiantes (column names: LONGITUDE_X and LATITUDE_Y) \cr FR: le data.frame contenant le degre (parametre de mobilite) et les coodonnees GPS (nom des colonnes : LONGITUDE_X et LATITUDE_Y).
 #' @param y  (par defaut, 0.8) the size of the localities. It can be a parameters contained in x.
+#' @param NAME  the column containing the names of the localities.
 #' @keywords plot ggplot sna network
 #' @usage mobilitymap(x,y)
-#'
 mobilitymap <- function(x, y = 0.8){
     z = y
   if(y != 0.8 & y %in% names(x))
